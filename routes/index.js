21e2de005141ae595537f6296d37955fb505b34b
warstick <manikanta.srirangam@gmail.com>
2019-01-17 22:43:32 -0800

    adding aws deploy

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My First Jenkins AWS EC2 Pipeline', subTitle: 'Mani' });
+  res.render('index', { title: 'My First Jenkins AWS EC2', subTitle: 'Mani' });
 });
 
 module.exports = router;
